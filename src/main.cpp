575dfbef0f753b416c7860cd6051a72adf12681e
JObersi10 <68297002+JObersi10@users.noreply.github.com>
2026-07-13 13:11:46 -0400

    fix: UnityW implicit cast for YoungestChildFlowCoordinatorOrSelf

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ using namespace AppleMusicSearch::UI;
 static void openBeatCrate() {
     static SafePtrUnity<FlowCoordinators::AppleMusicFlowCoordinator> fc;
     if (!fc) fc = BSML::Helpers::CreateFlowCoordinator<FlowCoordinators::AppleMusicFlowCoordinator*>();
-    auto* parent = BSML::Helpers::GetMainFlowCoordinator()->YoungestChildFlowCoordinatorOrSelf();
+    auto _w = BSML::Helpers::GetMainFlowCoordinator()->YoungestChildFlowCoordinatorOrSelf();
+    HMUI::FlowCoordinator* parent = _w;
     if (parent) parent->PresentFlowCoordinator(fc.ptr(), nullptr, HMUI::ViewController_AnimationDirection::Horizontal, false, false);
 }
 
